a72d11e50737cb019c4dcd58003ed9e07ede7064
andry360 <andry360@hotmail.it>
2025-02-13 15:34:57 +0100

    Creazione script ONT

---
+++ x_ONT.sh
@@ -0,0 +1,9 @@
+# Questo script serve per la ricerca delle informazioni e modifica degli apparati ONT.
+
+
+ip_SFP = 10.10.1.1
+ip_SFP_interface = 10.10.1.2
+ip_GATEWAY = 10.10.1.1
+ip_ROUTER = 192.168.1.1
+
+#CONTROLLA SE PUOI AGGIUNGERE IMMAGINI E DOCUMENTA CREAZIONE WAN
